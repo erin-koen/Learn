dccd0c7160c724d736e236fea54be6a4212b0560
Erin Koen <erin.koen@gmail.com>
2019-08-22 12:17:00 -0400

    dev environment sorted out

--- Udemy-DRF/02-WEB-APIs/04-Requests/00-requests.py
+++ Udemy-DRF/02-WEB-APIs/04-Requests/00-requests.py
@@ -1,12 +1,9 @@
 import requests
 
+
 def main():
     response = requests.get("http://www.google.com")
-    # response = requests.get("http://www.google.com/random-address/")
-    print("Status Code: ", response.status_code)
-    # print("Headers: ", response.headers)
-    # print("'Content-Type': ", response.headers['Content-Type'])
-    print("Content: ", response.text)
+    print(f'Status Code: {response.status_code}')
 
 
 if __name__ == "__main__":
