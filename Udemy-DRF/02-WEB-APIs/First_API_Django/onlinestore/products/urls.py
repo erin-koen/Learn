0ae5bd7e732e4a67a423264dffb2cdd6181dbba1
Erin Koen <erin.koen@gmail.com>
2019-08-26 17:15:22 -0400

    First part of Udemy course finished - how to build routes

--- Udemy-DRF/02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
+++ Udemy-DRF/02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductDetailView, ProductListView
+from .views import product_list, product_detail, manufacturer_detail, active_manufacturers
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
-    #specifiy the primary key of the Product in the url below
-    path("products/<int:pk>", ProductDetailView.as_view(), name="product-detail")
+    path("products/", product_list, name="product-list"),
+    path("products/<int:pk>", product_detail, name="product-detail"),
+    path("manufacturers/<int:pk>", manufacturer_detail, name="manufacturer-detail"),
+    path("manufacturers/active", active_manufacturers, name = "active-mfgs")
 ]
